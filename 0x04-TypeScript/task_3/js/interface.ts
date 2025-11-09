6bb43c3252d00a278377209a40baa4bdfb14e25e
Feyisara Agiri <mohrenny8@gmail.com>
2025-11-09 21:07:42 +0100

    Initialized a new directory

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,11 @@
+// task_3/js/interface.ts
+
+// Type for row IDs
+export type RowID = number;
+
+// Interface for row elements
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
