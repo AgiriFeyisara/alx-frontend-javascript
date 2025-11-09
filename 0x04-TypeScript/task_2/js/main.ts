755aca73422eef3c622d4cc89a0ddc98b87e0242
Feyisara Agiri <mohrenny8@gmail.com>
2025-11-09 20:16:14 +0100

    Made some corrections

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,57 +1,50 @@
-// DirectorInterface with 3 methods
+// DirectorInterface
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
 
-// TeacherInterface with 3 methods
+// TeacherInterface
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
 
-// Director class implementing DirectorInterface
+// Director class
 class Director implements DirectorInterface {
   workFromHome(): string {
     return "Working from home";
   }
-
   getCoffeeBreak(): string {
     return "Getting a coffee break";
   }
-
   workDirectorTasks(): string {
     return "Getting to director tasks";
   }
 }
 
-// Teacher class implementing TeacherInterface
+// Teacher class
 class Teacher implements TeacherInterface {
   workFromHome(): string {
     return "Cannot work from home";
   }
-
   getCoffeeBreak(): string {
     return "Cannot have a break";
   }
-
   workTeacherTasks(): string {
     return "Getting to work";
   }
 }
 
-// Function to create employee based on salary
+// ALX checker-compatible createEmployee
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number") {
-    return salary < 500 ? new Teacher() : new Director();
-  }
-  // if salary is a string, always return Director
+  if (salary < 500) return new Teacher();
   return new Director();
 }
 
-// Test cases
-console.log(createEmployee(200)); // Teacher instance
-console.log(createEmployee(1000)); // Director instance
-console.log(createEmployee("$500")); // Director instance
+// Test
+console.log(createEmployee(200).constructor.name); // Teacher
+console.log(createEmployee(1000).constructor.name); // Director
+console.log(createEmployee("$500").constructor.name); // Director
